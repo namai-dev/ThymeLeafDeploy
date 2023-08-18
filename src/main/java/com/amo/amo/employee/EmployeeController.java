98a67f80706c490ed8a42b1ec1e323614d9ec77c
namai-dev <namaiHarmony4@gmail.com>
2023-08-18 15:53:19 +0300

    update

--- src/main/java/com/amo/amo/employee/EmployeeController.java
+++ src/main/java/com/amo/amo/employee/EmployeeController.java
@@ -2,10 +2,7 @@ package com.amo.amo.employee;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,8 +24,10 @@ public class EmployeeController {
         return employeeService.employeeList();
     }
 
-    private void deleteEmployee(Long id){
+    @DeleteMapping
+    private String deleteEmployee(@RequestParam Long id){
         employeeService.deleteEmployee(id);
+        return "redirect:/";
     }
 
 
--- src/main/java/com/amo/amo/employee/model/ModelController.java
+++ src/main/java/com/amo/amo/employee/model/ModelController.java
@@ -1,12 +1,14 @@
 package com.amo.amo.employee.model;
 
 import com.amo.amo.employee.Employee;
+import com.amo.amo.employee.EmployeeRepo;
 import com.amo.amo.employee.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -15,6 +17,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ModelController {
     private final EmployeeService employeeService;
+    private final EmployeeRepo employeeRepo;
 
     @GetMapping({"/", "/listEmployee"})
     public ModelAndView modelAndView(){
@@ -30,6 +33,15 @@ public class ModelController {
         ModelAndView view = new ModelAndView("addEmployee");
         Employee newEmployee = new Employee();
         view.addObject("employee", newEmployee);
+        return view;
+    }
+
+
+    @GetMapping("/updateEmployeeForm")
+    public ModelAndView updateEmployee(@RequestParam Long employeeId){
+        ModelAndView view = new ModelAndView("update-form");
+        Employee employee = employeeRepo.findById(employeeId).get();
+        view.addObject("employee", employee);
         return view;
     }
 }
